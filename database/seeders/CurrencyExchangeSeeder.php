8097ad2f53a0260c09285136501c43064d084b8a
JoseMiguelFN7 <jferreira.5655@unimar.edu.ve>
2026-03-29 01:47:11 -0400

    Add: currency exchange seeder

---
+++ database/seeders/CurrencyExchangeSeeder.php
@@ -0,0 +1,212 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class CurrencyExchangeSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $rates = [
+            [
+                'id' => 1,
+                'currency_id' => 2,
+                'rate' => '396.3674',
+                'valid_at' => '2026-02-18 08:00:00',
+                'created_at' => '2026-02-17 19:40:37',
+                'updated_at' => '2026-02-17 19:40:37'
+            ],
+            [
+                'id' => 2,
+                'currency_id' => 2,
+                'rate' => '393.2216',
+                'valid_at' => '2026-02-13 19:57:50',
+                'created_at' => null,
+                'updated_at' => null
+            ],
+            [
+                'id' => 3,
+                'currency_id' => 2,
+                'rate' => '407.3786',
+                'valid_at' => '2026-02-24 08:00:00',
+                'created_at' => '2026-02-24 03:33:59',
+                'updated_at' => '2026-02-24 03:33:59'
+            ],
+            [
+                'id' => 4,
+                'currency_id' => 2,
+                'rate' => '411.0857',
+                'valid_at' => '2026-02-25 08:00:00',
+                'created_at' => '2026-02-25 06:40:38',
+                'updated_at' => '2026-02-25 06:40:38'
+            ],
+            [
+                'id' => 5,
+                'currency_id' => 2,
+                'rate' => '414.0455',
+                'valid_at' => '2026-02-26 08:00:00',
+                'created_at' => '2026-02-26 06:00:02',
+                'updated_at' => '2026-02-26 06:00:02'
+            ],
+            [
+                'id' => 6,
+                'currency_id' => 2,
+                'rate' => '417.3579',
+                'valid_at' => '2026-02-27 08:00:00',
+                'created_at' => '2026-02-27 06:00:02',
+                'updated_at' => '2026-02-27 06:00:02'
+            ],
+            [
+                'id' => 7,
+                'currency_id' => 2,
+                'rate' => '419.9873',
+                'valid_at' => '2026-03-02 08:00:00',
+                'created_at' => '2026-02-28 06:00:03',
+                'updated_at' => '2026-02-28 06:00:03'
+            ],
+            [
+                'id' => 8,
+                'currency_id' => 2,
+                'rate' => '421.8772',
+                'valid_at' => '2026-03-03 08:00:00',
+                'created_at' => '2026-03-03 07:32:30',
+                'updated_at' => '2026-03-03 07:32:30'
+            ],
+            [
+                'id' => 9,
+                'currency_id' => 2,
+                'rate' => '425.6741',
+                'valid_at' => '2026-03-04 08:00:00',
+                'created_at' => '2026-03-04 06:00:02',
+                'updated_at' => '2026-03-04 06:00:02'
+            ],
+            [
+                'id' => 10,
+                'currency_id' => 2,
+                'rate' => '427.9302',
+                'valid_at' => '2026-03-05 08:00:00',
+                'created_at' => '2026-03-05 06:00:05',
+                'updated_at' => '2026-03-05 06:00:05'
+            ],
+            [
+                'id' => 11,
+                'currency_id' => 2,
+                'rate' => '431.0113',
+                'valid_at' => '2026-03-06 08:00:00',
+                'created_at' => '2026-03-06 06:00:05',
+                'updated_at' => '2026-03-06 06:00:05'
+            ],
+            [
+                'id' => 12,
+                'currency_id' => 2,
+                'rate' => '433.1664',
+                'valid_at' => '2026-03-09 08:00:00',
+                'created_at' => '2026-03-07 06:00:04',
+                'updated_at' => '2026-03-07 06:00:04'
+            ],
+            [
+                'id' => 13,
+                'currency_id' => 2,
+                'rate' => '438.2050',
+                'valid_at' => '2026-03-11 08:00:00',
+                'created_at' => '2026-03-11 06:00:06',
+                'updated_at' => '2026-03-11 06:00:06'
+            ],
+            [
+                'id' => 14,
+                'currency_id' => 2,
+                'rate' => '443.2587',
+                'valid_at' => '2026-03-13 04:00:00',
+                'created_at' => '2026-03-13 02:00:04',
+                'updated_at' => '2026-03-13 02:00:04'
+            ],
+            [
+                'id' => 15,
+                'currency_id' => 2,
+                'rate' => '446.8048',
+                'valid_at' => '2026-03-16 04:00:00',
+                'created_at' => '2026-03-14 02:00:03',
+                'updated_at' => '2026-03-14 02:00:03'
+            ],
+            [
+                'id' => 16,
+                'currency_id' => 2,
+                'rate' => '448.3686',
+                'valid_at' => '2026-03-17 04:00:00',
+                'created_at' => '2026-03-17 02:00:02',
+                'updated_at' => '2026-03-17 02:00:02'
+            ],
+            [
+                'id' => 17,
+                'currency_id' => 2,
+                'rate' => '451.5072',
+                'valid_at' => '2026-03-18 04:00:00',
+                'created_at' => '2026-03-18 02:00:04',
+                'updated_at' => '2026-03-18 02:00:04'
+            ],
+            [
+                'id' => 18,
+                'currency_id' => 2,
+                'rate' => '455.2547',
+                'valid_at' => '2026-03-20 04:00:00',
+                'created_at' => '2026-03-20 00:41:57',
+                'updated_at' => '2026-03-20 00:41:57'
+            ],
+            [
+                'id' => 19,
+                'currency_id' => 2,
+                'rate' => '457.0757',
+                'valid_at' => '2026-03-23 04:00:00',
+                'created_at' => '2026-03-23 14:30:10',
+                'updated_at' => '2026-03-23 14:30:10'
+            ],
+            [
+                'id' => 20,
+                'currency_id' => 2,
+                'rate' => '459.4525',
+                'valid_at' => '2026-03-24 04:00:00',
+                'created_at' => '2026-03-24 01:31:09',
+                'updated_at' => '2026-03-24 01:31:09'
+            ],
+            [
+                'id' => 21,
+                'currency_id' => 2,
+                'rate' => '462.6687',
+                'valid_at' => '2026-03-25 04:00:00',
+                'created_at' => '2026-03-25 02:00:02',
+                'updated_at' => '2026-03-25 02:00:02'
+            ],
+            [
+                'id' => 22,
+                'currency_id' => 2,
+                'rate' => '466.6014',
+                'valid_at' => '2026-03-26 04:00:00',
+                'created_at' => '2026-03-26 02:00:02',
+                'updated_at' => '2026-03-26 02:00:02'
+            ],
+            [
+                'id' => 23,
+                'currency_id' => 2,
+                'rate' => '471.7004',
+                'valid_at' => '2026-03-30 04:00:00',
+                'created_at' => '2026-03-28 02:00:02',
+                'updated_at' => '2026-03-28 02:00:02'
+            ],
+            [
+                'id' => 24,
+                'currency_id' => 2,
+                'rate' => '468.6100',
+                'valid_at' => '2026-03-27 04:00:00',
+                'created_at' => '2026-03-28 14:05:49',
+                'updated_at' => '2026-03-28 14:05:49'
+            ]
+        ];
+
+        DB::table('currency_exchanges')->insert($rates);
+    }
+}
