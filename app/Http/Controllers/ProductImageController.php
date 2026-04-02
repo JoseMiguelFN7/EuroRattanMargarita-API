bd1491b42d8eabb100aac328dc99009d8827a9a0
JoseMiguelFN7 <jferreira.5655@unimar.edu.ve>
2026-04-02 10:09:55 -0400

    Fix: added use str in ProductImageController

--- app/Http/Controllers/ProductImageController.php
+++ app/Http/Controllers/ProductImageController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\ProductImage;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Str;
 
 class ProductImageController extends Controller
 {
